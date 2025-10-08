4a2805aba8a7e58b9ea89a732d6784e95f460995
Josiah Culling <jcull36@wgu.edu>
2025-10-08 18:35:11 -0400

    Add prompt functionality and error handling. Add user role and parts (genai.type.Contents)

--- main.py
+++ main.py
@@ -7,23 +7,26 @@ def main():
     print("Hello from ai-agent-bootdev!")
 
     arg = sys.argv
-    print("this is the name of the program:", arg[0])
 
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
-
     client = genai.Client(api_key=api_key)
-
-    response = client.models.generate_content(
-        model="gemini-2.0-flash-001", 
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    )
-
-    print(response.text)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    messages = [
+        genai.types.Contents(role="user", parts=[types.Part(text=user_prompt)])
+    ]
 
 
+    try:
+        response = client.models.generate_content(
+            model="gemini-2.0-flash-001", 
+            contents=messages
+        )
+        print(response.text)
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    except (IndexError, UnboundLocalError):
+        print("Please provide an input within double quotes (\"\")")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
